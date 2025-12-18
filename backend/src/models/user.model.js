777941bb85205ffbd7f2ffe465c11ae729bf0d8a
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-12-18 12:56:53 +0530

    schema is done

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,24 +1,34 @@
 import mongoose from 'mongoose'
-import {z} from 'zod'
 
-const schema=new mongoose.schema({
-    name:{
+const schema=new mongoose.Schema({
+    username:{
         required:true,
         type:String,
-        unique:true,
-    
+        trim:true,
+
+
     },
     password:{
         required:true,
         type:String,
-        unique:true,
-
+        minlength:8,
     },
     email:{
         required:true,
         type:String,
         unique:true,
-        
+        validate:{
+        validator:function(v){
+            return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v)
+
+        },
+       message: props => `${props.value} is not a valid email`
 
     }
-},timestamps:true)
+
+    },
+
+},{timestamps:true})
+
+const User=mongoose.model("user",schema);
+export default User;
