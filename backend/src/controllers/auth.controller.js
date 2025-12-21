eabe340f82cd0b2caf8783b90779e33719478b54
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-12-22 00:28:57 +0530

    fix error

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,23 +1,88 @@
-export const Signup=async(req,res)=>{
-    return res.status(403).json({
-        message:"signup route"
-    })
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+import generateTokenAndSetCookie from "../lib/util/generateToken.js";
 
-}
+export const Signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        error: "Invalid email format"
+      });
+    }
+    const userExist = await User.findOne({ username });
+    if (userExist) {
+      return res.status(400).json({
+        error: "Username is already taken"
+      });
+    }
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.status(400).json({
+        error: "Email is already taken"
+      });
+    }
 
-export const Login=async(req,res)=>{
-    return res.status(403).json({
-        message:"login route"
-    })
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    });
 
-}
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-export const Logout=async(req,res)=>{
-    return res.status(403).json({
-        message:"Logout route"
-    })
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
 
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-}
+export const Login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const Logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
